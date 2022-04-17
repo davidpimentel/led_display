0b579823f484f10cfeec054c3e90424c33669ca2
David Pimentel <david.pimentel65@gmail.com>
2022-04-17 17:34:17 +0100

    conditional rendering

--- screens/base_screen.py
+++ screens/base_screen.py
@@ -13,6 +13,7 @@ class BaseScreen(Thread):
         self.data = None
         self.data_lock = Lock()
         self.last_data_time = None
+        self.should_render = True
 
     def stop(self):
         self.stop_run.set()
@@ -27,20 +28,27 @@ class BaseScreen(Thread):
     def run_data(self):
         if self.fetch_data_delay() is not None and not self.is_data_thread_running() and (not self.last_data_time or (time.time() - self.last_data_time) > self.fetch_data_delay()):
             self.last_data_time = time.time()
-            self.data_thread = Thread(target=self.fetch_data, daemon=True)
+            self.data_thread = Thread(target=self.run_fetch_data, daemon=True)
             self.data_thread.start()
 
     def is_data_thread_running(self):
         return self.data_thread is not None and self.data_thread.is_alive()
 
     def run_render(self):
-        if not self.last_render_time or (time.time() - self.last_render_time) > self.render_delay():
-              self.render()
-              self.last_render_time = time.time()
+        if self.render_delay() is not None and (not self.last_render_time or (time.time() - self.last_render_time) > self.render_delay()):
+            self.should_render = True
+
+        if self.should_render:
+            self.render()
+            self.last_render_time = time.time()
+            self.should_render = False
 
     def set_data(self, data):
         with self.data_lock:
+            old_data = self.data
             self.data = data
+            if (data != old_data):
+                self.should_render = True
 
     def get_data(self):
         return_data = None
@@ -48,12 +56,15 @@ class BaseScreen(Thread):
             return_data = self.data
         return return_data
 
+    def run_fetch_data(self):
+        data = self.fetch_data()
+        self.set_data(data)
+
 
     # Methods to override in subclass
 
     def fetch_data(self):
         pass
-        print("fetch data")
 
     def fetch_data_delay(self):
         return None
@@ -62,4 +73,4 @@ class BaseScreen(Thread):
         raise Exception("render() not implemented")
 
     def render_delay(self):
-        raise Exception("delay_seconds() not implemented")
+        return None
--- screens/weather/__init__.py
+++ screens/weather/__init__.py
@@ -15,28 +15,22 @@ class Screen(BaseScreen):
         self.font = FONTS["5x8"]
         self.white = COLORS["white"]
         self.offscreen_canvas = self.matrix.CreateFrameCanvas()
-        self.blink = True
-
-    def render_delay(self):
-        return 0.5
 
     def fetch_data_delay(self):
-        return 5
+        return 30
 
     def fetch_data(self):
         weather = get_current_weather()
         feels_like_temp = str(int(weather["main"]["feels_like"]))
-        self.set_data(feels_like_temp)
+        return feels_like_temp
 
     def render(self):
         self.offscreen_canvas.Clear()
         feels_like_temp = self.get_data()
 
-        if feels_like_temp is not None and self.blink:
+        if feels_like_temp is not None:
             graphics.DrawText(
                 self.offscreen_canvas, self.font, 5, 28, self.white, feels_like_temp + "°"
             )
-
-        self.blink = not self.blink
 
         self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
