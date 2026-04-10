f95c47280b7625bde8910dc2a557f6d34ac42f62
David Pimentel <david.pimentel65@gmail.com>
2026-04-10 15:49:43 -0400

    refactored gym count

--- screens/gym_count/__init__.py
+++ screens/gym_count/__init__.py
@@ -1,4 +1,3 @@
-import json
 import subprocess
 from dataclasses import dataclass
 
@@ -7,55 +6,54 @@ from lib.fonts import FONTS
 from lib.weather import get_current_weather
 from PIL import Image
 from rgbmatrix import graphics
-from screens.base_screen import BaseScreen
+from screens.stateful_screen import StatefulScreen
 
 
 @dataclass
-class Data:
-    people_at_gym: str
-    feels_like_temp: str
+class GymCountState:
+    people_at_gym: str = ""
+    feels_like_temp: str = ""
 
-class Screen(BaseScreen):
+
+class Screen(StatefulScreen[GymCountState]):
     def __init__(self):
-        super().__init__()
+        super().__init__(initial_state=GymCountState())
         self.vital_logo = Image.open("./images/vital_logo.png")
         self.font = FONTS["6x9"]
         self.white = COLORS["white"]
         self.green = COLORS["green"]
 
-    def fetch_data_interval(self):
-        return 30
+    def setup(self):
+        self.create_interval(self._fetch_data, seconds=30)
 
-    def fetch_data(self):
+    def _fetch_data(self):
         people_at_gym = str(
             subprocess.check_output(
                 [
-                  "curl",
-                  "--silent",
-                  "https://display.safespace.io/value/live/a7796f34"
+                    "curl",
+                    "--silent",
+                    "https://display.safespace.io/value/live/a7796f34",
                 ]
             ).decode("utf-8")
         )
-        weather = get_current_weather("40.722518", "-73.954734") # Vital location
+        weather = get_current_weather("40.722518", "-73.954734")  # Vital location
         feels_like_temp = str(int(weather["current"]["feels_like"]))
-        return Data(
+        self.set_state(
             people_at_gym=people_at_gym,
-            feels_like_temp=feels_like_temp
+            feels_like_temp=feels_like_temp,
         )
 
-    def get_color_for_range(self, count):
-        if count < 100:
-            return self.green
+    def _render(self, canvas, state: GymCountState):
+        if not state.people_at_gym:
+            return
 
-    def render(self, canvas, data):
-        if data is not None:
-            canvas.SetImage(
-                self.vital_logo.convert("RGB"), offset_x=3, offset_y=3
-            )
+        canvas.SetImage(
+            self.vital_logo.convert("RGB"), offset_x=3, offset_y=3
+        )
 
-            graphics.DrawText(
-                canvas, self.font, 8, 28, self.green, data.people_at_gym
-            )
-            graphics.DrawText(
-                canvas, self.font, 42, 28, self.white, data.feels_like_temp + "°"
-            )
+        graphics.DrawText(
+            canvas, self.font, 8, 28, self.green, state.people_at_gym
+        )
+        graphics.DrawText(
+            canvas, self.font, 42, 28, self.white, state.feels_like_temp + "°"
+        )
