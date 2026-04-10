91ce40241ddacc73bceefa605b037c5e476c553f
David Pimentel <david.pimentel65@gmail.com>
2026-04-10 16:20:51 -0400

    refactored stretching routine

--- screens/stretching_routine/__init__.py
+++ screens/stretching_routine/__init__.py
@@ -1,39 +1,73 @@
 import json
-from math import floor
 import time
-from rgbmatrix import graphics
-from screens.base_screen import BaseScreen
-from lib.view_helper.text import TextOscillator, right_align_text
-from lib.fonts import FONTS
+from dataclasses import dataclass, field
+from math import floor
+from typing import Optional
+
 from lib.colors import COLORS
+from lib.fonts import FONTS
+from lib.view_helper.text import TextOscillator, right_align_text
+from rgbmatrix import graphics
+from screens.stateful_screen import StatefulScreen
 
 
-class Screen(BaseScreen):
+@dataclass
+class StretchingState:
+    current_exercise: Optional[dict] = None
+    is_resting: bool = True
+    rest_time_start: float = 0.0
+    exercise_start: float = 0.0
+    done: bool = False
+
+
+class Screen(StatefulScreen[StretchingState]):
     def __init__(self, config="{}"):
-        super().__init__(display_indefinitely=True)
-        self.config = json.loads(config)
+        super().__init__(
+            initial_state=StretchingState(rest_time_start=time.time()),
+            display_indefinitely=True,
+        )
+        self.rest_duration = 5
+        self.exercises_iter = iter(json.loads(config))
         self.font = FONTS["5x8"]
         self.white = COLORS["white"]
-        self.rest_duration = 5
-        self.is_resting = True
-        self.rest_time_start = time.time()
-        self.exercises = self.config
-        self.exercises_iter = iter(self.exercises)
-        self.current_exercise = None
-        self.next_exercise()
-        self.exercise_start = time.time()
         self.exercise_name_scroller = TextOscillator(
             self.font, self.white, self.font.height, 3
         )
+        self._next_exercise()
 
-    def animation_interval(self):
-        return 1 / 32
+    def setup(self):
+        self.create_interval(self._tick, seconds=1 / 32)
 
-    def display_duration(self):
-        return super().display_duration()
+    def _next_exercise(self):
+        exercise = next(self.exercises_iter, None)
+        if exercise is None:
+            self.set_state(done=True, current_exercise=None)
+        else:
+            self.set_state(current_exercise=exercise)
 
-    def render(self, canvas, data):
-        if self.current_exercise is None:
+    def _tick(self):
+        state = self.get_state()
+        if state.done:
+            return
+
+        if state.is_resting:
+            rest_left = floor(self.rest_duration - (time.time() - state.rest_time_start))
+            if rest_left <= 0:
+                self.set_state(is_resting=False, exercise_start=time.time())
+            else:
+                self.set_state()
+        else:
+            time_left = floor(
+                state.current_exercise["duration"] - (time.time() - state.exercise_start)
+            )
+            if time_left <= 0:
+                self._next_exercise()
+                self.set_state(is_resting=True, rest_time_start=time.time())
+            else:
+                self.set_state()
+
+    def _render(self, canvas, state: StretchingState):
+        if state.done:
             graphics.DrawText(
                 canvas,
                 self.font,
@@ -44,10 +78,13 @@ class Screen(BaseScreen):
             )
             return
 
-        self.exercise_name_scroller.update_text(self.current_exercise["name"])
+        if state.current_exercise is None:
+            return
+
+        self.exercise_name_scroller.update_text(state.current_exercise["name"])
         self.exercise_name_scroller.render(canvas)
 
-        if self.is_resting:
+        if state.is_resting:
             graphics.DrawText(
                 canvas,
                 self.font,
@@ -56,36 +93,17 @@ class Screen(BaseScreen):
                 self.white,
                 "in...",
             )
+            countdown = floor(self.rest_duration - (time.time() - state.rest_time_start))
+        else:
+            countdown = floor(
+                state.current_exercise["duration"] - (time.time() - state.exercise_start)
+            )
 
         right_align_text(
             canvas,
-            str(self.rest_time_left() if self.is_resting else self.time_left()),
+            str(countdown),
             self.font,
             self.white,
             canvas.height - self.font.height,
             5,
         )
-
-        self.update_state()
-
-    def update_state(self):
-        if self.is_resting:
-            if self.rest_time_left() <= 0:
-                self.is_resting = False
-                self.exercise_start = time.time()
-        else:
-            if self.time_left() <= 0:
-                self.next_exercise()
-                self.is_resting = True
-                self.rest_time_start = time.time()
-
-    def rest_time_left(self):
-        return floor(self.rest_duration - (time.time() - self.rest_time_start))
-
-    def time_left(self):
-        return floor(
-            self.current_exercise["duration"] - (time.time() - self.exercise_start)
-        )
-
-    def next_exercise(self):
-        self.current_exercise = next(self.exercises_iter, None)
