a8dee550402e8af503d35e94f2c3cbff5c6d9c65
David Pimentel <david.pimentel65@gmail.com>
2026-04-10 16:17:54 -0400

    refactored spotify

--- screens/spotify/__init__.py
+++ screens/spotify/__init__.py
@@ -1,28 +1,25 @@
-import os
 from dataclasses import dataclass
+from typing import Optional
 
-import requests
 import spotipy
 from lib.colors import COLORS
 from lib.fonts import FONTS
 from lib.view_helper.text import TextOscillator
-from PIL import Image
 from rgbmatrix import graphics
-from screens.base_screen import BaseScreen
-from spotipy.cache_handler import CacheFileHandler
+from screens.stateful_screen import StatefulScreen
 from spotipy.oauth2 import SpotifyOAuth
 
 
 @dataclass
-class Data:
-    artist_name: str
-    song_name: str
-    album_image_url: str
+class SpotifyState:
+    artist_name: str = ""
+    song_name: str = ""
+    is_playing: bool = False
 
 
-class Screen(BaseScreen):
+class Screen(StatefulScreen[SpotifyState]):
     def __init__(self, username=None):
-        super().__init__(display_indefinitely=True)
+        super().__init__(initial_state=SpotifyState(), display_indefinitely=True)
         scope = "user-read-currently-playing"
         self.font = FONTS["4x6"]
         self.white = COLORS["white"]
@@ -41,44 +38,37 @@ class Screen(BaseScreen):
             font=self.font, text_color=self.gray, position_y=20, delay=1
         )
 
-    def fetch_data_interval(self):
-        return 5
+    def setup(self):
+        self.create_interval(self._fetch_spotify, seconds=5)
+        self.create_interval(self._animate, seconds=0.05)
 
-    def fetch_data(self):
+    def _fetch_spotify(self):
         currently_playing = self.sp.currently_playing()
 
         if currently_playing is None:
-            return None
+            self.set_state(is_playing=False)
+            return
 
         item = currently_playing["item"]
-        album_image_url = item["album"]["images"][2]["url"]
         artist_name = item["artists"][0]["name"]
         song_name = item["name"]
 
-        return Data(
+        self.set_state(
             artist_name=artist_name,
             song_name=song_name,
-            album_image_url=album_image_url,
+            is_playing=True,
         )
 
-    def animation_interval(self):
-        return 0.05
+    def _animate(self):
+        self.set_state()
 
-    def render(self, canvas, data):
-
-        if data is None:
+    def _render(self, canvas, state: SpotifyState):
+        if not state.is_playing:
             graphics.DrawText(canvas, self.font, 4, 10, self.white, "NOTHING PLAYING")
             return
 
-            # im = Image.open(requests.get(album_image_url, stream=True).raw)
-            # im = im.resize((32, 32), Image.NEAREST)
-
-            # canvas.SetImage(
-            #     im.convert("RGB"), offset_x=3, offset_y=3
-            # )
-
-        self.song_scroller.update_text(data.song_name)
-        self.artist_scroller.update_text(data.artist_name)
+        self.song_scroller.update_text(state.song_name)
+        self.artist_scroller.update_text(state.artist_name)
 
         self.song_scroller.render(canvas)
         self.artist_scroller.render(canvas)
