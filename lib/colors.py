583da22c3d60ccf919a5b3b9cb6a5a491f0fcaf6
David Pimentel <david.pimentel65@gmail.com>
2026-04-12 10:29:33 -0400

    refactored color and fonts

--- lib/colors.py
+++ lib/colors.py
@@ -1,9 +1,36 @@
 from rgbmatrix import graphics
 
+
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+        self._graphics_color = graphics.Color(r, g, b)
+
+    def dimmed(self, factor):
+        return Color(int(self.r * factor), int(self.g * factor), int(self.b * factor))
+
+    @property
+    def graphics(self):
+        return self._graphics_color
+
+
+class Colors:
+    white = Color(255, 255, 255)
+    gray = Color(128, 128, 128)
+    green = Color(134, 216, 80)
+    teal = Color(89, 216, 215)
+    yellow = Color(244, 193, 21)
+    red = Color(255, 0, 0)
+    blue = Color(0, 0, 255)
+
+
+# Backward compat
 COLORS = {
-    "white": graphics.Color(255, 255, 255),
-    "gray": graphics.Color(128, 128, 128),
-    "green": graphics.Color(134, 216, 80),
-    "teal": graphics.Color(89, 216, 215),
-    "yellow": graphics.Color(244, 193, 21),
+    "white": Colors.white.graphics,
+    "gray": Colors.gray.graphics,
+    "green": Colors.green.graphics,
+    "teal": Colors.teal.graphics,
+    "yellow": Colors.yellow.graphics,
 }
--- lib/fonts.py
+++ lib/fonts.py
@@ -1,17 +1,35 @@
 from rgbmatrix import graphics
 
 
+class FontRegistry:
+    _cache = {}
+
+    @classmethod
+    def get(cls, name):
+        if name not in cls._cache:
+            font = graphics.Font()
+            font.LoadFont(f"./fonts/{name}.bdf")
+            cls._cache[name] = font
+        return cls._cache[name]
+
+    @classmethod
+    def text_width(cls, name, text):
+        font = cls.get(name)
+        return sum(font.CharacterWidth(c) for c in bytearray(text.encode("utf-8")))
+
+
 def get_font(font_path):
     font = graphics.Font()
     font.LoadFont(font_path)
     return font
 
 
+# Backward compat
 FONTS = {
     "4x6": get_font("./fonts/4x6.bdf"),
     "6x9": get_font("./fonts/6x9.bdf"),
     "7x13": get_font("./fonts/7x13.bdf"),
     "5x8": get_font("./fonts/5x8.bdf"),
     "6x10": get_font("./fonts/6x10.bdf"),
-    "6x12": get_font("./fonts/6x12.bdf")
+    "6x12": get_font("./fonts/6x12.bdf"),
 }
