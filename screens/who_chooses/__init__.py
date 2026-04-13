b6614eeb07c54935bd149f2a3fc07072c21574f0
David Pimentel <david.pimentel65@gmail.com>
2026-04-13 13:08:54 -0400

    who chooses refactor

--- screens/who_chooses/__init__.py
+++ screens/who_chooses/__init__.py
@@ -1,10 +1,10 @@
 import random
 from dataclasses import dataclass
 
-from lib.colors import COLORS
-from lib.fonts import FONTS
+from lib.colors import Colors
+from lib.fonts import FontRegistry
+from lib.ui import Img, Positioned, Stack, Text
 from PIL import Image
-from rgbmatrix import graphics
 from screens.base_screen import BaseScreen
 
 
@@ -16,26 +16,18 @@ class WhoChoosesState:
 class Screen(BaseScreen[WhoChoosesState]):
     def __init__(self):
         super().__init__(initial_state=WhoChoosesState(rand_int=random.randint(0, 1)))
-        self.font = FONTS["5x8"]
-        self.font_height = self.font.height
-        self.white = COLORS["white"]
-        self.green = COLORS["green"]
-        self.teal = COLORS["teal"]
-        self.images = [
-            (Image.open("images/dave.png").convert("RGB"), "DAVE", self.green),
-            (Image.open("images/nicole.png").convert("RGB"), "NICOLE", self.teal),
+        self.choices = [
+            (Image.open("images/dave.png").convert("RGB"), "DAVE", Colors.green),
+            (Image.open("images/nicole.png").convert("RGB"), "NICOLE", Colors.teal),
         ]
 
-    def render(self, canvas, state: WhoChoosesState):
-        image, name, name_color = self.images[state.rand_int]
+    def build(self, state: WhoChoosesState):
+        image, name, name_color = self.choices[state.rand_int]
+        choose_width = FontRegistry.text_width("5x8", "CHOOSE:")
 
-        graphics.DrawText(
-            canvas, self.font, 0, self.font_height, self.white, "YOU"
-        )
-        length = graphics.DrawText(
-            canvas, self.font, 0, (self.font_height * 2), self.white, "CHOOSE:"
-        )
-        graphics.DrawText(
-            canvas, self.font, 0, (self.font_height * 3), name_color, name
-        )
-        canvas.SetImage(image, length + 1, 0)
+        return Stack(children=[
+            Positioned(x=0, y=0, child=Text("YOU", font="5x8", color=Colors.white)),
+            Positioned(x=0, y=8, child=Text("CHOOSE:", font="5x8", color=Colors.white)),
+            Positioned(x=0, y=16, child=Text(name, font="5x8", color=name_color)),
+            Positioned(x=choose_width + 1, y=0, child=Img(image)),
+        ])
